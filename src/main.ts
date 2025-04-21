17a703b4f52c76cd38325d2b7052531c2735bae0
mctursh <adenugaayoadehenry63@gmail.com>
2025-04-21 16:12:40 +0100

    Enable CORS in main.ts to allow requests from localhost:3000 with specified methods and headers

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: '*',
+    credentials: false,
+  });
+
   await app.listen(4600);
 }
 bootstrap();
